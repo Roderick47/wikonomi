b62eefeefaa436eab0519e370e526e78e4e0fcb0
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-08-20 12:34:44 +1000

    Add welcome onboarding routes

--- Home/urls.py
+++ Home/urls.py
@@ -5,4 +5,7 @@ app_name="Home"
 urlpatterns = [
     path('',views.HomeView,name='home'),
     path('About',views.AboutView,name="about"),
+    path('welcome/', views.WelcomeView, name='welcome'),
+    path('welcome/continue/', views.WelcomeContinueView, name='welcome-continue'),
+    path('welcome/explore/', views.WelcomeExploreView, name='welcome-explore'),
 ]
